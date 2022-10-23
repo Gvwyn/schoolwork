fca87503198b25c1493930368f31f518a7dbeb57
Gvwyn <kovacstormabalazskrisztofer@gmail.com>
2022-10-23 18:45:36 +0200

    unnecessary parantheses

--- finished/python/listaskis/listaskis10.py
+++ finished/python/listaskis/listaskis10.py
@@ -5,6 +5,6 @@ for x in range(20): lista.append(random.randint(0, 20))
 
 n = random.randint(1, 5)
 nszamok.append(lista[:n])
-nszamok.append((lista[-n:]))
+nszamok.append(lista[-n:])
 
 print(f'{lista}, {n} -> {nszamok}')
